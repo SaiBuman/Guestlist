f24b1bad902bcf873e44de8e708103509bf9a66a
Sai Buman <78092854+SaiBuman@users.noreply.github.com>
2021-07-05 13:12:40 +0530

    Add files via upload

---
+++ ADVC80/main.js
@@ -0,0 +1,13 @@
+function submit() {
+    var guest_name = document.getElementById("namesofguest").value ;
+    names_of_guest.push(guest_name);
+}
+
+               
+function sort() {
+    names_of_guest.sort();
+}
+
+function search() {
+  var s = document.getElementById("")
+}
